0749eeaca1d6aad39fc0bcdecf18edfe3b4e72ec
Shi, Jennifer (js868a) <js868a@us.att.com>
2019-06-27 15:05:57 -0400

    Kory update 6/27 part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  navRoute: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  nav(uri: string) {
+    this.navRoute = uri;
+  }
 }
